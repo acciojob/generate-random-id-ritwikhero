9397f0f33ddd74fbbe6658eddfab845bcfc61a74
Ritwik Singh <76203605+ritwikhero@users.noreply.github.com>
2024-03-31 23:15:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function makeid(l) {
   // write your code here
+	const lookUp = "abcdefghijklmnop1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";	
+
+	let password = "";
+
+	for(let i=1; i<=l; i++){
+		let randomIndex = Math.floor(Math.random()*lookUp.length);
+		let randomCharacter = lookUp.charAt(randomIndex);
+		password += randomCharacter;
+	}
+	return password;
+
+	
 }
 
 // Do not change the code below.
